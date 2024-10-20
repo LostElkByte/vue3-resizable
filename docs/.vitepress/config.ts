f194f6f34572500a9c83e3b8b39c77436af723eb
LostElk <lostelk.email@gmail.com>
2024-10-21 04:59:49 +0800

    修改文档 github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     sidebar: [],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/LostElkByte/vue3-resizable" },
     ],
   },
 })
